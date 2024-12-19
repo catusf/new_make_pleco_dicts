cfab091e7c253d9242c6d3a0385112a9b1c01030
Catus Phan <catus.phan@gmail.com>
2024-12-19 09:55:12 +0800

    Update max cpus

--- scrape_hanzii_net-second.py
+++ scrape_hanzii_net-second.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     cpu_count = os.cpu_count()
     spared_cpus = 5 if is_running_on_windows() else 0
-    max_workers = max(1, cpu_count - spared_cpus)  # Leave 2 cores free
+    max_workers = max(3, cpu_count - spared_cpus)  # Leave 2 cores free
     print(f"Using {max_workers} threads for parallel processing")
 
     new_urls_list = sorted(new_urls, reverse=True)
